41dc4a4cc3b704f6d6624451f6ca2279dc944ecf
sa46lll <9812smh@naver.com>
2023-11-30 18:25:37 +0900

    feat: kiosk에서 상품을 선택하는 메서드를 추가한다

---
+++ src/main/java/org/sa46lll/Application.java
@@ -0,0 +1,10 @@
+package org.sa46lll;
+
+import org.sa46lll.kiosk.TddKiosk;
+
+public class Application {
+
+    public static void main(String[] args) {
+        new TddKiosk();
+    }
+}
--- src/test/java/org/sa46lll/kiosk/KioskTest.java
+++ src/test/java/org/sa46lll/kiosk/KioskTest.java
@@ -3,6 +3,7 @@ package org.sa46lll.kiosk;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.sa46lll.product.Product;
 
 class KioskTest {
 
@@ -27,18 +28,19 @@ class KioskTest {
     @Test
     void 키오스크에_상품이_존재한다() {
         Kiosk kiosk = new TddKiosk();
-        List<Product> menus = kiosk.getProduct();
+        List<Product> products = kiosk.getProducts();
 
-        List<String> menuNames = menus.stream()
+        List<String> productNames = products.stream()
                 .map(Product::getName)
                 .toList();
 
-        Assertions.assertThat(menuNames).contains("아메리카노");
+        Assertions.assertThat(productNames).contains("아메리카노");
     }
 
     @Test
     void 상품을_선택하면_상세_정보가_반환된다() {
         Kiosk kiosk = new TddKiosk();
+
         Product product = kiosk.selectProduct("아메리카노");
 
         Assertions.assertThat(product.getPrice()).isPositive();
