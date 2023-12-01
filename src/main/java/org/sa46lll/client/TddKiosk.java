d50c63de255180b4f651b55da5b9d677b5e3fac1
sa46lll <9812smh@naver.com>
2023-12-01 16:28:40 +0900

    refactor: kiosk 메서드 역할 분리

--- src/main/java/org/sa46lll/client/TddKiosk.java
+++ src/main/java/org/sa46lll/client/TddKiosk.java
@@ -16,22 +16,34 @@ public class TddKiosk implements Kiosk {
 
     @Override
     public void run() {
-        OutputView.printWelcomeMessage(cafe.getName());
-        OutputView.printProducts(products);
-        String order = InputView.askOrder();
-        Product product = selectProduct(order);
+        printStartMessage();
+        Product product = askOrder();
         OutputView.printSelectedInfo(product);
     }
 
+    private void printStartMessage() {
+        OutputView.printWelcomeMessage(cafe.getName());
+        OutputView.printProducts(products);
+    }
+
+    private Product askOrder() {
+        String order = InputView.askOrder();
+        return selectProduct(order);
+    }
+
     @Override
-    public Product selectProduct(String name) {
+    public Product selectProduct(String product) {
+        validate(product);
+        return this.products.stream()
+                .filter(p -> p.getName().equals(product))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 상품입니다."));
+    }
+
+    private static void validate(String name) {
         if (name == null) {
             throw new IllegalArgumentException("메뉴를 입력해 주세요.");
         }
-        return this.products.stream()
-                .filter(p -> p.getName().equals(name))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 상품입니다."));
     }
 
     private List<Product> loadProducts() {
