90cf5b35ecc92bbfeec09b805b08f950695933f4
sa46lll <9812smh@naver.com>
2023-12-01 16:10:15 +0900

    refactor: kiosk 클래스 위치 수정

--- src/main/java/org/sa46lll/client/OutputView.java
+++ src/main/java/org/sa46lll/client/OutputView.java
@@ -8,17 +8,17 @@ public class OutputView {
     private static final String WELCOME_MESSAGE = "에 오신 것을 환영합니다.";
     private static final String SELECTED_INFO_MESSAGE = "%s는 %d원 입니다.";
 
-    public static void printWelcomeMessage(String store) {
+    static void printWelcomeMessage(String store) {
         System.out.println(store + WELCOME_MESSAGE);
     }
 
-    public static void printProducts(List<Product> products) {
+    static void printProducts(List<Product> products) {
         products.forEach(
                 product -> System.out.println("- " + product.getName())
         );
     }
 
-    public static void printSelectedInfo(Product product) {
+    static void printSelectedInfo(Product product) {
         System.out.printf(SELECTED_INFO_MESSAGE, product.getName(), product.getPrice());
     }
 }
--- src/test/java/org/sa46lll/client/KioskTest.java
+++ src/test/java/org/sa46lll/client/KioskTest.java
@@ -1,4 +1,4 @@
-package org.sa46lll.kiosk;
+package org.sa46lll.client;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
