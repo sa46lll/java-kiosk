c1a7e1db77bc2a3e2f686609ba4374516db03ad6
sa46lll <9812smh@naver.com>
2023-12-01 16:00:42 +0900

    refactor: input, output 관리하는 클래스 분리

---
+++ src/main/java/org/sa46lll/client/OutputView.java
@@ -0,0 +1,24 @@
+package org.sa46lll.client;
+
+import java.util.List;
+import org.sa46lll.product.Product;
+
+public class OutputView {
+
+    private static final String WELCOME_MESSAGE = "에 오신 것을 환영합니다.";
+    private static final String SELECTED_INFO_MESSAGE = "%s는 %d원 입니다.";
+
+    public static void printWelcomeMessage(String store) {
+        System.out.println(store + WELCOME_MESSAGE);
+    }
+
+    public static void printProducts(List<Product> products) {
+        products.forEach(
+                product -> System.out.println("- " + product.getName())
+        );
+    }
+
+    public static void printSelectedInfo(Product product) {
+        System.out.printf(SELECTED_INFO_MESSAGE, product.getName(), product.getPrice());
+    }
+}
