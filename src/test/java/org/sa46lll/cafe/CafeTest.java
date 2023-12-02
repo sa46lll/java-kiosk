fea04ea3bf079fd840ccdb3319714c15fba009e4
sa46lll <9812smh@naver.com>
2023-12-03 00:07:52 +0900

    test: 테스트 코드 optional 적용

--- src/test/java/org/sa46lll/cafe/CafeTest.java
+++ src/test/java/org/sa46lll/cafe/CafeTest.java
@@ -2,7 +2,6 @@ package org.sa46lll.cafe;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.sa46lll.client.Kiosk;
 import org.sa46lll.client.TddKiosk;
@@ -25,7 +24,7 @@ class CafeTest {
     void 상품의_사이즈와_수량을_선택하여_주문한다() {
         Product product = kiosk.mapToProduct("아메리카노");
 
-        Order order = cafe.order(product, 3, CoffeeSize.TALL);
+        Order order = cafe.order(product, 3, CoffeeSize.TALL).orElse(null);
 
         System.out.println(order.getQuantity());
         Assertions.assertThat(order.getQuantity()).isEqualTo(3);
@@ -36,7 +35,7 @@ class CafeTest {
     void 주문한_상품의_가격을_알_수_있다() {
         Product product = kiosk.mapToProduct("아메리카노");
 
-        Order order = cafe.order(product, 3, CoffeeSize.TALL);
+        Order order = cafe.order(product, 3, CoffeeSize.TALL).orElse(null);
 
         Assertions.assertThat(order.getTotalPrice()).isEqualTo(product.getPrice() * 3);
     }
@@ -45,8 +44,8 @@ class CafeTest {
     void 주문_번호는_연속적으로_증가해야_한다() {
         Product product = kiosk.mapToProduct("아메리카노");
 
-        Order order1 = cafe.order(product, 1, CoffeeSize.TALL);
-        Order order2 = cafe.order(product, 2, CoffeeSize.GRANDE);
+        Order order1 = cafe.order(product, 1, CoffeeSize.TALL).orElse(null);
+        Order order2 = cafe.order(product, 2, CoffeeSize.GRANDE).orElse(null);
 
         Assertions.assertThat(order2.getOrderNumber() - order1.getOrderNumber()).isEqualTo(1);
     }
@@ -55,7 +54,7 @@ class CafeTest {
     void 주문_생성_시간을_알_수_있다() {
         Product product = kiosk.mapToProduct("아메리카노");
 
-        Order order = cafe.order(product, 1, CoffeeSize.TALL);
+        Order order = cafe.order(product, 1, CoffeeSize.TALL).orElse(null);
 
         Assertions.assertThat(order.getCreatedAt()).isNotNull();
     }
